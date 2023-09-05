eee3319204d072de9670fb1fef5249542d339520
Hyan Campos <hyan@worc.com.br>
2023-09-05 10:32:34 -0300

    feat: created modal search component

---
+++ src/components/Modals/styles.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+`;
+
+export const Caption = styled.p`
+  font-size: 12px;
+  padding-top: 10px;
+`;
+export const Title = styled.h3`
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+export const Description = styled.p`
+  font-size: 14px;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+`;
+
+export const ContentIconClose = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+
+  svg {
+    cursor: pointer;
+  }
+`;
+
+export const GoogleImage = styled.img`
+  width: 80px;
+`;
