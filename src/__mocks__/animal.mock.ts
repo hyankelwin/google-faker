ca2b0f9f0ef1126d9e4661b313fed283b84bbe1a
Hyan Campos <hyan@worc.com.br>
2023-09-05 10:46:02 -0300

    test: created animals mock to tests

---
+++ src/__mocks__/animal.mock.ts
@@ -0,0 +1,37 @@
+const mockAnimal = {
+  id: 1,
+  name: 'Siamês',
+  image:
+    'https://inovaveterinaria.com.br/wp-content/uploads/2016/01/Gato-siames-INOVA-scaled.jpg',
+  description: 'This cat is the most beautiful than all cats',
+  site: 'http://google.com.br',
+};
+
+const mockAnimalList = [
+  {
+    id: 1,
+    name: 'Siamês',
+    image:
+      'https://inovaveterinaria.com.br/wp-content/uploads/2016/01/Gato-siames-INOVA-scaled.jpg',
+    description: 'This cat is the most beautiful than all cats',
+    site: 'http://google.com.br',
+  },
+  {
+    id: 2,
+    name: 'Maine Coon',
+    image:
+      'https://inovaveterinaria.com.br/wp-content/uploads/2016/01/Maine-Coon-INOVA-scaled.jpg',
+    description: 'This cat is more furry than others',
+    site: 'http://youtube.com.br',
+  },
+  {
+    id: 3,
+    name: 'Angorá',
+    image:
+      'https://inovaveterinaria.com.br/wp-content/uploads/2016/01/portrait-two-white-american-longhair-cats-black-white-background.jpg',
+    description: 'This cat is all white',
+    site: 'http://uol.com.br',
+  },
+];
+
+export { mockAnimal, mockAnimalList };
